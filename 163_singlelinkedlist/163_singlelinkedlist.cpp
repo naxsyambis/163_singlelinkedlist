338dde23246bedd22f69a3ada6259731987e1ea7
naxsyambis <naxsyambis@gmail.com>
2024-04-25 11:25:03 +0100

    membuat syarat if

--- 163_singlelinkedlist/163_singlelinkedlist.cpp
+++ 163_singlelinkedlist/163_singlelinkedlist.cpp
@@ -20,4 +20,15 @@ void addNode() {
 	nodeBaru->noMhs = nim;
 	nodeBaru->name = nama;
 
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
 }
